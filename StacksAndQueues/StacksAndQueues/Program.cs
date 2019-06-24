11961278cb2311e19fd580df6793b93e2a3af5e5
richard0923 <r.rosadoocasio@gmail.com>
2019-06-24 12:51:43 -0700

    added a lot

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using StacksAndQueues.Classes;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Stack myStack = new Stack(5);
+        }
+    }
+}
