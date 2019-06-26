a44e8a4e22a233dfdc7ad9ee68c8e298de560c82
richard0923 <r.rosadoocasio@gmail.com>
2019-06-26 12:59:16 -0700

    adding animalshelter

---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/AnimalShelter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter
+{
+    class AnimalShelter
+    {
+
+        Queue<Cats> cats = new Queue<Cats>
+    }
+}
