6552a2653db254da9a318753cd1018a408b3238d
richard0923 <r.rosadoocasio@gmail.com>
2019-07-01 08:54:23 -0700

    adding things still need to finish

--- Data-structures/Tree/Tree/Classes/Node.cs
+++ Data-structures/Tree/Tree/Classes/Node.cs
@@ -6,5 +6,14 @@ namespace Tree.Classes
 {
     class Node
     {
+
+        public int Value { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
     }
 }
--- Data-structures/Tree/Tree/Classes/Tree.cs
+++ Data-structures/Tree/Tree/Classes/Tree.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tree.Classes;
 
 namespace Tree.Classes
 {
     class Tree
     {
+
+        public Node Root;
+
+        public Tree()
+        {
+            Root = null;
+        }
+
+        //methods these use recursion 
+        public int[] Preorder()
+        {
+
+        }
+
+        public int[] Inorder()
+        {
+
+        }
+
+        public int[] Postorder()
+        {
+
+        }
+
+        class BinarySearchTree
+        {
+            public void Add()
+            {
+
+            }
+
+            public void Contains()
+            {
+
+            }
+        }
     }
 }
