39f05c55e9213e4ae77fd2424f7253a4504fd57a
richard0923 <r.rosadoocasio@gmail.com>
2019-07-15 13:08:35 -0700

    added project

---
+++ challenges/InsertSortAlgo/InsertAlgoTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace InsertAlgoTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public static void Test1()
+        {
+            int[] unsorted = { 20, 47, 2, 7, 19 };
+            int[] sorted = { 2, 7, 19, 20, 47 };
+
+            InsertionSort()
+        }
+    }
+}
