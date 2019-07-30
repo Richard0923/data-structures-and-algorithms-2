86894188524de059a7def2b4f433d73a3aabba39
richard0923 <r.rosadoocasio@gmail.com>
2019-07-30 13:59:30 -0700

    added classes for get edges

---
+++ challenges/Get_Edges/Get_Edges/Classes/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Get_Edges.Classes
+{
+    public class Vertex<T>
+    {
+        public T Value { get; set; }
+        public Vertex(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ challenges/Get_Edges/Get_Edges/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Get_Edges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void GetEdges()
+        {
+            
+        }
+    }
+}
