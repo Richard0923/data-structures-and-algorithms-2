e84ac20c029b2d7163b7824d3364e055fff66266
richard0923 <r.rosadoocasio@gmail.com>
2019-07-31 10:35:04 -0700

    added whiteboard still needto finish methods

--- challenges/Get_Edges/Get_Edges/Program.cs
+++ challenges/Get_Edges/Get_Edges/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Get_Edges.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Get_Edges
 {
@@ -9,9 +12,37 @@ namespace Get_Edges
             Console.WriteLine("Hello World!");
         }
 
-        public void GetEdges()
+        public void GetEdges(Graph<string> graph, string[] pathArray)
         {
+            List<Vertex<string>> visted = new List<Vertex<string>>();
+            Queue<Vertex<string>> breadth = new Queue<Vertex<string>>();
+
+            //vertex to hold the value of the input array
+            Vertex<string> arrayValue = new Vertex<string>(pathArray[0]);
+
+            if (graph.AdjacencyList.ContainsKey(arrayValue))
+            {
+                breadth.Enqueue(arrayValue);
+            }
             
+            while(breadth.TryPeek(out arrayValue))
+            {
+                Vertex<string> front = breadth.Dequeue();
+
+                if (visted.Contains(front))
+                {
+                    continue;
+                }
+                visted.Add(front);
+
+                foreach (var nieghbors in graph.AdjacencyList[front])
+                {
+                    if (!visted.Contains(nieghbors))
+                    {
+                        //need to grab the price and if  there is a path to 
+                    }
+                }
+            }
         }
     }
 }
---
+++ challenges/Get_Edges/README.md
@@ -0,0 +1,19 @@
+# Challenge Summary
+<!-- Short summary or background information -->
+Create a Method called that takes in a Graph and an Array of city names. If there is a direct path to the citys in that array return a bool and the wieght of that path.
+## Challenge Description
+<!-- Description of the challenge -->
+Write a function which takes in a graph, and an array of city names. Without utilizing any of the built-in methods available to your 
+language, return whether the full trip is possible with direct flights, and how much it would cost.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O:
+
+Time: O(n) n = The number of elements in that input array.
+
+Space: O(1) I am not creating new memory.
+
+## Solution
+<!-- Embedded whiteboard image -->
+![WhiteBoard](/assets/getedgesWB.jpg)
